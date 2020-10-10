e76b3970e026bb4c85aa33b5fada740e62c9bd4f
伊藤拓海 <itoutakumi@mbp.local>
2020-10-10 21:28:48 +0900

    create html and server

---
+++ main.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var html = require('fs').readFileSync('index.html');
+
+http.createServer(function(req, res) {
+  if(req.method === 'GET') {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.end(html);
+  } else if(req.method === 'POST') {
+    var data = '';
+    
+   //POSTデータを受けとる
+   req.on('data', function(chunk) {data += chunk})
+      .on('end', function() {
+ 
+        console.log(data);
+        res.end(html);
+ 
+      })
+ 
+   }
+}).listen(3000);
