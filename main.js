ef04d274c931fe4e0a4b879deed60d9803b5320b
伊藤拓海 <itoutakumi@mbp.local>
2020-10-16 13:00:12 +0900

    function add #1

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var html = require('fs').readFileSync('index.html');
 
+let todoList = [];
+
 http.createServer(function(req, res) {
   if(req.method === 'GET') {
     res.writeHead(200, {'Content-Type' : 'text/html'});
@@ -8,14 +10,17 @@ http.createServer(function(req, res) {
   } else if(req.method === 'POST') {
     var data = '';
     
-   //POSTデータを受けとる
-   req.on('data', function(chunk) {data += chunk})
-      .on('end', function() {
- 
-        console.log(data);
-        res.end(html);
- 
-      })
- 
-   }
+    req.on('data', function(chunk) {data += chunk})
+    .on('end', function() {
+      let todoItem = data.substr(5)
+      todoList.push(todoItem)
+      for(let i=0; i<todoList.length; i++){
+        todoList[i]="<div class=\"todoItem\">"+todoList[i]+"</div>"
+      }
+      html = html.toString().replace(/<div class="todoitem"><\/div>/g,todoList[todoList.length-1]+'<div class="todoitem"></div>')
+      res.end(html);
+      
+    })
+    
+  }
 }).listen(3000);
