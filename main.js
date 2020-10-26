3225f819968540fd18930050bb8ea781035112d0
伊藤拓海 <itoutakumi@mbp.local>
2020-10-26 17:22:50 +0900

    add function delete

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ let todoList = [];
 //テーブルの初期化
 connection.query('truncate table todoitems;', function (err, rows, fields) {
   if (err) { console.log('err: ' + err); } 
-  console.log('DELETED');
 });
 
 http.createServer(function(req, res) {
@@ -26,25 +25,43 @@ http.createServer(function(req, res) {
       todoItem=decodeURIComponent(todoItem.replace(/\+/g, "%20"));
 
       if(todoItem.slice(0,9) === "od=DELETE"){
-        console.log("SUCCESSED")
+        let id=todoItem.slice(13)
+        connection.query('SELECT * FROM todoitems;', function (err, rows, fields) {
+          if (err) { console.log('err: ' + err); } 
+          let del;
+          if(rows[id]){
+            del = '<div class="todoItem">'+rows[id-1].item+'</div><form method="POST" action="main.js"><input type="submit" value="削除" /><input type="hidden" name="_method" value="DELETE" /><input type="hidden" name="id" value="'+id+'" /></form>'
+            html = html.toString().replace(del,"");
+            console.log(del)
+          }else if(rows[id-1]){
+            del = '<div class="todoItem">'+rows[id-1].item+'</div><form method="POST" action="main.js"><input type="submit" value="削除" /><input type="hidden" name="_method" value="DELETE" /><input type="hidden" name="id" value="'+id+'" /></form>'
+            html = html.toString().replace(del,"");
+            console.log(del)
+          }
+        });
+        connection.query('DELETE FROM todoitems WHERE id='+id+';', function (err, rows, fields) {
+          if (err) { console.log('err: ' + err); } 
+        
+          console.log('DELETED!!!!')
+        });
         res.end(html);
         return;
       }
 
       connection.query('insert into todoitems(item, isdone, creation, deadline) values (\' ' + todoItem + '\', 0, now(), now());', function (err, rows, fields) {
         if (err) { console.log('err: ' + err); } 
-        console.log('YATAAAAAA');
       });
       
       connection.query('SELECT * FROM todoitems;', function (err, rows, fields) {
         if (err) { console.log('err: ' + err); } 
         for(let i=0; i<rows.length; i++){
-          todoList[i]="<div class=\"todoItem\">"+rows[i].item+"</div><form method=\"POST\" action=\"main.js\"><input type=\"submit\" value=\"削除\" /><input type=\"hidden\" name=\"_method\" value=\"DELETE\" /><input type=\"hidden\" name=\"id\" value=\"takcheru\" /></form>"
+          todoList[i]="<div class=\"todoItem\">"+rows[i].item+"</div><form method=\"POST\" action=\"main.js\"><input type=\"submit\" value=\"削除\" /><input type=\"hidden\" name=\"_method\" value=\"DELETE\" /><input type=\"hidden\" name=\"id\" value=\""+rows[i].id+"\" /></form>"
         }
         html = html.toString().replace(/<div class="todoitem"><\/div>/g,todoList[todoList.length-1]+'<div class="todoitem"></div>')
+        res.writeHead(303, { 'Location': '/' });
         res.end(html);
       });
-    })
+    }) 
     
   }
 }).listen(3000);
