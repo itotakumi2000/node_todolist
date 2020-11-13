aad2a00dd244dbdf75e36a001186fbd12ae8d6b9
伊藤拓海 <itoutakumi@mbp.local>
2020-11-13 14:47:41 +0900

    brush up the code

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var http = require('http');
 var connection = require('./mysql');
 var express = require("express");
 var app = express();
@@ -15,11 +14,11 @@ connection.query('truncate table todoitems;', function (err, rows, fields) {
 //GETリクエスト
 app.get('/', function (req, res) {
 
-  var data = {
+  give_data = {
     method: "get"
   };
   
-  res.render('./index.ejs', data);
+  res.render('./index.ejs', give_data);
 })
 
 //POSTリクエスト
@@ -28,7 +27,8 @@ app.post('/post', function (req, res) {
   
   req.on('data', function(chunk) {data += chunk})
   .on('end', function() {
-    let todoItem = data.substr(5)
+    let Item_position = 5
+    let todoItem = data.substr(Item_position)
     //POSTの内容をデコード、日本語と空白に対応
     todoItem=decodeURIComponent(todoItem.replace(/\+/g, "%20"));
     
@@ -50,18 +50,18 @@ app.post('/post', function (req, res) {
   }) 
 });
 
-
-
 // //DELETEリクエスト
 app.post('/delete', function (req, res){
   var data_delete = '';
 
   req.on('data', function(chunk) {data_delete += chunk})
   .on('end', function() {
-    let delete_id = data_delete.substr(3)
+    let delete_id_position = 3
+    let delete_id = data_delete.substr(delete_id_position)
     //POSTの内容をデコード、日本語と空白に対応
     delete_id=decodeURIComponent(delete_id.replace(/\+/g, "%20"));
 
+    //データを削除
     connection.query('DELETE FROM todoitems WHERE id='+delete_id+';', function (err, rows, fields) {
       if (err) { console.log('err: ' + err); } 
     });
@@ -89,10 +89,15 @@ app.post('/put', function (req, res){
     //POSTの内容をデコード、日本語と空白に対応
     data_put=decodeURIComponent(data_put.replace(/\+/g, "%20"));
     let and_index = data_put.indexOf("&")
-    let put_id = data_put.substr(and_index + 4)
-    let content = data_put.substr(5, and_index - 5)
 
+    let put_id_position = and_index + 4;
+    let put_id = data_put.substr(put_id_position)
 
+    let content_position_start = 5;
+    let number_to_cut = and_index - 5;
+    let content = data_put.substr(content_position_start, number_to_cut)
+
+    //データを更新
     connection.query('UPDATE todoitems SET item=\"'+content+'\" WHERE id='+put_id+';', function (err, rows, fields) {
       if (err) { console.log('err: ' + err); } 
     });
